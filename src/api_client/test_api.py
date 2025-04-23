e5a5cf5496b572c908584c60ec389584efbb89c4
Gwhite <guilhem.dolet@gmail.com>
2025-04-23 11:45:18 +0200

    test_api

--- src/api_client/test_api.py
+++ src/api_client/test_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = "https://eprel.ec.europa.eu/api/products/waterheaters/2199809"
 
@@ -6,7 +7,9 @@ response = requests.get(url)
 
 if response.status_code == 200:
     print("✅ API accessible sans authentification !")
-    print(response.json())  
+    # print(response.json())
+    print(json.dumps(response.json(), indent=2))
+    # json.dumps
 else:
     print(f"❌ Erreur {response.status_code}")
     print(response.text)  # Voir le détail de l'erreur
