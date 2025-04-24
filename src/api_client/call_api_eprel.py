7861d5ab7267b4530425f8607c1eae0aac2f063a
Guilhem Dolet <151618197+GuilhemDolet@users.noreply.github.com>
2025-04-24 16:06:00 +0200

    PR : ajout du script pour appeler l'api (#5)
    * call_api_eprel.py premier test ok
    
    * Update call_api_eprel
    
    * iso gitignore
    
    * appelle api et stockage des jsons en local ok
    
    * Création du fichier ndjson regroupant tous les jsons reçu via l'api

--- src/api_client/call_api_eprel.py
+++ src/api_client/call_api_eprel.py
@@ -4,21 +4,24 @@ import json
 from time import sleep
 
 # Ma liste d'id produit à récupérer dynamiquement depuis le scrapping 
-product_ids = [2199799, 2199809, 2199832]
+product_ids = []
+base_url = "https://eprel.ec.europa.eu/api/products/waterheaters/"
 
 # Dossier de destination (pour l'instant en local mais futur DL)
 output_folder = "src/api_client/data/raw"
 # os.makedirs(output_folder, exist_ok=True)
 
-base_url = "https://eprel.ec.europa.eu/api/products/waterheaters/"
+with open("src/EPREL_scrapper/data/urls.json", "r") as f:
+    urls = json.load(f)
 
-for product_id in product_ids : 
+for url in urls : 
+    product_id = url[-7:]
     builded_url = f"{base_url}{product_id}"
-    print(builded_url)
 
     try:
-        response = requests.get(builded_url)
 
+        response = requests.get(builded_url)
+        print("ok")
         if response.status_code == 200 :
             data = response.json()
             output_path = os.path.join(output_folder, f"{product_id}.json")
--- src/api_client/test_api.py
+++ src/api_client/test_api.py
@@ -1,15 +1,54 @@
 import requests
 import json
 
-url = "https://eprel.ec.europa.eu/api/products/waterheaters/2199809"
+# url = "https://eprel.ec.europa.eu/api/products/waterheaters/2199809"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-if response.status_code == 200:
-    print("✅ API accessible sans authentification !")
-    # print(response.json())
-    print(json.dumps(response.json(), indent=2))
-    # json.dumps
-else:
-    print(f"❌ Erreur {response.status_code}")
-    print(response.text)  # Voir le détail de l'erreur
+# if response.status_code == 200:
+#     print("✅ API accessible sans authentification !")
+#     # print(response.json())
+#     print(json.dumps(response.json(), indent=2))
+#     # json.dumps
+# else:
+#     print(f"❌ Erreur {response.status_code}")
+#     print(response.text)  # Voir le détail de l'erreur
+
+
+import requests
+import os
+import json
+from time import sleep
+
+# Ma liste d'id produit à récupérer dynamiquement depuis le scrapping 
+product_ids = [2199799, 2199809, 2199832]
+
+# Dossier de destination (pour l'instant en local mais futur DL)
+output_folder = "src/api_client/data/raw"
+# os.makedirs(output_folder, exist_ok=True)
+
+base_url = "https://eprel.ec.europa.eu/api/products/waterheaters/"
+
+for product_id in product_ids : 
+    builded_url = f"{base_url}{product_id}"
+    print(builded_url)
+    print(type(builded_url))
+    try:
+        response = requests.get(builded_url)
+
+        if response.status_code == 200 :
+            data = response.json()
+            output_path = os.path.join(output_folder, f"{product_id}.json")
+            print(output_path)
+            with open(output_path, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+
+            print(f"json {product_id} enregistré")
+
+        else:
+            print(f"bug : {response.status_code}")
+
+        sleep(2)
+
+    except Exception as e:
+        print(f"Try to call the api and then: {e}")
